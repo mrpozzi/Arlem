6255ead23fb1bbd9dbcb4135e6db838540ef00d9
Luca Pozzi <lucapozzi@Lucas-MBP.attlocal.net>
2018-12-10 21:16:09 -0800

    Fixed T

--- harlem/parameters/init_T.py
+++ harlem/parameters/init_T.py
@@ -4,24 +4,19 @@ from harlem.utils import SQRT_DBL_EPSILON
 
 def init_T(full_data, Qc, dGn, delta_star, x_grid, v_grid):
 
-    w, dGn = zip(*sorted(zip(full_data.w, dGn)))
+    w, dGn = np.array(sorted(zip(full_data.w, dGn))).T
 
     T1 = np.zeros((len(x_grid), len(v_grid)))
     T2 = np.zeros((len(x_grid), len(v_grid)))
 
-    # TODO: Make this WAY faster
     for i, x in enumerate(x_grid):
-        print("{} ".format(i/len(x_grid)), end="", flush=True)
         for j, v in enumerate(v_grid):
 
             if v > x + delta_star:
-                denominator = 0.0
-                k = 0
 
-                while (k < len(w)) and (w[k] < v):
-                    if (x < w[k]):
-                        denominator += dGn[k] * Qc(v - w[k])
-                    k += 1
+                if v > x + delta_star:
+                    mask_w = (x < w) & (w < v)
+                    denominator = (dGn[mask_w] * Qc(v - w[mask_w])).sum()
 
                 if denominator > SQRT_DBL_EPSILON:
                     T1[i, j] = (v - x) / denominator
